3972ed5630036d268e5e1f79b8d6bc09890246ee
arudappagaarisoumya-web <arudappagaarisoumya@gmail.com>
2026-08-08 18:22:09 +0530

    Add files via upload

---
+++ day2_python_basics.py
@@ -0,0 +1,25 @@
+#variables and datatypes
+name="SOUMYA"
+age=20
+percentage=79.6
+student=True
+print("Name:",name)
+print("Age:",age)
+print("Percentage:",percentage)
+print("Student:",student)
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(student))
+#operators
+a=10
+b=20
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Molulus:",a%b)
+#functions
+def greet(name):
+    print("Hello", name)
+greet("Sowmya")
